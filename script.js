a649e756a1382d1e4a4015d67829adb63e374cd8
jbprojects95 <158042318+jbprojects95@users.noreply.github.com>
2024-06-26 10:39:06 +0100

    outlined basic html, css and js for calculator

---
+++ script.js
@@ -0,0 +1,34 @@
+let firstNumber;
+let secondNumber;
+let operator;
+
+const warning = "Type a valid operator";
+
+const add = (a, b) => {
+  return a + b;
+};
+
+const subtract = (a, b) => {
+  return a - b;
+};
+
+const multiply = (a, b) => {
+  return a * b;
+};
+
+const divide = (a, b) => {
+  return a / b;
+};
+
+function operate(op, n1, n2) {
+  switch (op) {
+    case "+":
+      return add(n1, n2);
+    case "-":
+      return subtract(n1, n2);
+    case "*":
+      return multiply(n1, n2);
+    case "/":
+      return divide(n1, n2);
+  }
+}
