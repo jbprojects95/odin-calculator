b3ea416673b86010ef56f2c973a902fa31e4e693
jbprojects95 <158042318+jbprojects95@users.noreply.github.com>
2024-07-08 11:04:52 +0100

    Added code to process the displayValue array

--- script.js
+++ script.js
@@ -20,15 +20,68 @@ const divide = (a, b) => {
   return a / b;
 };
 
-function operate(op, n1, n2) {
+function operate(op) {
   switch (op) {
     case "+":
-      return add(n1, n2);
+      return add(firstNumber, secondNumber);
     case "-":
-      return subtract(n1, n2);
+      return subtract(firstNumber, secondNumber);
     case "*":
-      return multiply(n1, n2);
+      return multiply(firstNumber, secondNumber);
     case "/":
-      return divide(n1, n2);
+      return divide(firstNumber, secondNumber);
   }
 }
+
+const clearScreen = () => {
+  calcScreen.textContent = "";
+};
+
+const deleteDisplayValue = () => {
+  displayValue.length = 0;
+};
+
+const numSelect = document.querySelectorAll(".btn");
+const calcScreen = document.querySelector(".calc-screen");
+let displayValue = [];
+const numberValues = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+const ops = ["+", "-", "*", "/"];
+
+numSelect.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (numberValues.includes(button.value)) {
+      displayValue.push(Number(button.value));
+    } else {
+      displayValue.push(button.value);
+    }
+    calcScreen.textContent = displayValue.join("");
+    console.log(displayValue);
+
+    if (button.value === "c") {
+      clearScreen();
+      deleteDisplayValue();
+    }
+
+    if (button.value === "=") {
+      const indexOfEqual = displayValue.indexOf("=");
+      displayValue.splice(indexOfEqual);
+      for (const element of displayValue) {
+        if (typeof element !== "number") {
+          const indexed = displayValue.indexOf(element);
+          operator = [element];
+          firstNumber = Number(displayValue.slice(0, indexed).join(""));
+          secondNumber = Number(displayValue.slice(indexed + 1).join(""));
+          operator = operator.join("");
+        }
+      }
+      //put calculate function here
+      console.log(firstNumber);
+      console.log(secondNumber);
+      console.log(operator);
+    }
+  });
+});
+
+// const input = displayValue.join("");
+// const numbers = input.split(/\D/g);
+// const op = input.split(/\d/g).filter(Boolean);
